5c8214f206e5c6827c460ff4e6750486fcc67fad
nastya-atro <belle.nastja@gmail.com>
2021-05-23 22:26:45 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddList from './components/AddList/AddButtonList';
+import List from './components/List/List';
+import db from './assets/db.json'
+import Tasks from './components/Tasks/Tasks';
+
 
 function App() {
+
+  const [lists, setLists] = useState(db.lists.map(item => {
+    item.colors = db.colors.filter(color => color.id === item.colorId)[0].name
+    return item
+  }))
+
+  const onAddList = (newList) => {
+    const addedList = [
+      ...lists, newList
+    ]
+    setLists(addedList)
+  }
+
+  const removeList = () => {
+    alert('list remove')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo'>
+      <div className='todo_sidebar'>
+        <List items={[
+          { icon: <i class="fas fa-align-justify"></i>, name: 'Все задачи' }]} />
+        <List removeList={removeList} isRemovable items={lists} />
+        <AddList onAddList={onAddList} colors={db.colors} />
+      </div>
+      <div className='todo_main'>
+        <Tasks/>
+      </div>
+
     </div>
   );
 }
